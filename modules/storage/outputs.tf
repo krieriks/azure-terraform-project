9d4b761b97ea55ba0a6f556e2463bd92fee960c3
krieriks <krieriks@stud.ntnu.no>
2024-11-13 19:28:49 +0100

    added comments

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,3 +1,5 @@
+# This file contains the output variables for the storage module.
+
 output "storage_account_name" {
   description = "The name of the storage account."
   value       = azurerm_storage_account.storage_account.name
